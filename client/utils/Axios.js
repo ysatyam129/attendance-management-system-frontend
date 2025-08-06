cd71f214d5c82252f61363d5adb359e09b07b44c
ysatyam129 <yadavsatyamsingh078@gmail.com>
2025-08-06 19:16:56 +0530

    cor done

--- client/utils/Axios.js
+++ client/utils/Axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const Axios = axios.create({
-  baseURL: "http://localhost:8080/api/v1",
+  baseURL: "https://attendance-mangement.vercel.app/api/v1",
   withCredentials: true
 });
 export default Axios;
